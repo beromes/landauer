61bd9ab187744f26a358ecc0ff7d43a004829a73
beromes <nunesbernardo2000@gmail.com>
2023-03-26 12:34:22 -0300

    fix: refatorando codigo e melhorando reproducao

--- src/landauer/genetic_algorithm.py
+++ src/landauer/genetic_algorithm.py
@@ -7,14 +7,50 @@ import landauer.graph as graph
 import networkx as nx
 import random
 import json
+from operator import attrgetter
 
+'''
+Classes/Modelos
+'''
+class ParamMap:
+    name = 'Padrao'
+    w_energy = 1
+    w_delay = 0
+    n_generations = 1000
+    n_initial_individuals = 20
+    reproduction_rate = 1.0
+    mutation_rate = 0.2
+
+    def __init__(self, dictt):
+        if dictt is dict:          
+            for key in dictt:
+                setattr(self, key, dictt[key])
+
+class Individual:
+    dna = None
+    score = 0
+    evaluation = None
+
+    def __init__(self, dna):
+        self.dna = dna
+
+'''
+Funcoes auxiliares
+'''
 def calc_delay(aig):
     return len(nx.dag_longest_path(aig)) - 2
 
-def genetic_algorithm(aig, w_energy, w_delay, n_generations = 10, n_initial_individuals = 8, reproduction_rate = 1, mutation_rate = 0.2):
+
+'''
+Etapas algoritmo genetico
+'''
+def genetic_algorithm(aig, params):
+
+    # Converte dicionario em classe
+    params = ParamMap(params)
 
     # Valida entradas
-    if w_delay + w_energy != 1:
+    if params.w_delay + params.w_energy != 1:
         raise ValueError("A soma dos pesos deve ser igual a 1")
 
     # Simula circuito
@@ -22,116 +58,122 @@ def genetic_algorithm(aig, w_energy, w_delay, n_generations = 10, n_initial_indi
 
     # Calcula energia e profundidade iniciais
     initial_energy = evaluate.evaluate(aig, simulation)['total']
-    # initial_delay = calc_delay(aig)
+    initial_delay = calc_delay(aig)
 
-    # print('Energia e Delay inciais')
-    # print(initial_energy)
-    # print(initial_delay)
+    print('Energia e Delay inciais')
+    print(initial_energy)
+    print(initial_delay)
 
     # Retorna populacao inicial
-    def initial_population(aig, n_individuals):
+    def init_population(aig, n_individuals):
         assignment = framework.assignment(aig)
         population = []
 
         for i in range(0, n_individuals):
-            population.append(framework.randomize(aig, assignment))
+            new_individual = Individual(framework.randomize(aig, assignment))
+            population.append(new_individual)
 
         return population
 
+    # Funcao fitness que tenta minimizar a entropia
+    def fit(population):
+        for p in population:
+            forwarding_ = framework.forwarding(aig, p.dna)
+            evaluation = evaluate.evaluate(forwarding_, simulation)
+            p.score = 1 - (evaluation['total'] / initial_energy)
+
     # Faz reprodução dos individuos de uma populacao
-    def reproduce(population, scores, rate):
-        n_children = len(population) * rate
+    def reproduce(population, rate, min_score):
+        n_children = int(len(population) * rate)
         children = []
 
         for i in range(0, n_children):
             
+            #lambda_fun = lambda p: p.score - min_score + 1
+            #lambda_fun = lambda p: p.score
+            lambda_fun = lambda p: p.score / min_score
+
             # Escolhe os parentes
-            min_score = min(scores)
-            weights = list(map(lambda s: s - min_score + 1, scores))            
+            weights = list(map(lambda_fun, population))
             parents = random.choices(population, weights=weights, k=2)
 
             # Recombina os genes
-            child = parents[0].copy() # Filho inicialmente é a copia do primeiro pai
+            dna1 = parents[0].dna
+            dna2 = parents[1].dna
+            child = dna1.copy() # Filho inicialmente é a copia do primeiro pai
+
             for gate, input_ in list(child.keys()):
                 # Lista candidatos para uma determinada tupla
                 candidates = list(framework.candidates(aig, child, gate, input_))
-                
-                # Verifica se tambem pode puxar o gene do outro parente
-                if (parents[1][(gate, input_)] in candidates):
-                    options = (parents[0][(gate, input_)], parents[1][(gate, input_)])
-                    child[(gate, input_)] = random.choice(options)
 
-            children.append(child)
+                # Verifica se tambem pode puxar o gene do outro parente
+                if (dna2[(gate, input_)] in candidates):
+                    options = (dna1[(gate, input_)], dna2[(gate, input_)])
+                    child[(gate, input_)] = random.choice(options)
+                '''
+                if (dna1[(gate, input_)] == dna2[(gate, input_)]):
+                    iguais +=1
+                elif (child[(gate, input_)] == dna1[(gate, input_)]):
+                    pai1 += 1
+                elif (child[(gate, input_)] == dna2[(gate, input_)]):
+                    pai2 +=1
+                else:
+                    print('Anomalia!!!')
+                '''
+                        
+            children.append(Individual(child))
 
         return children
 
     # Aplica mutacao nos individuos de uma populacao
-    def mutate(population, rate):        
+    def mutate(population, rate):
         for i in population:
             [ should_mutate ] = random.choices((True, False), weights=(rate, 1 - rate), k=1)
             if should_mutate:
-                gate, input_ = random.choice(list(i.keys()))
-                candidates = list(framework.candidates(aig, i, gate, input_))
-                i[(gate, input_)] = random.choice(candidates)
-
+                gate, input_ = random.choice(list(i.dna.keys()))
+                candidates = list(framework.candidates(aig, i.dna, gate, input_))
+                i.dna[(gate, input_)] = random.choice(candidates)
         return population
 
-    # Funcao fitness
-    # def fit(population):
-    #     scores = []
-    #     for p in population:            
-    #         # Avalia individuo
-    #         forwarding_ = framework.forwarding(aig, p)
-    #         evaluation = evaluate.evaluate(forwarding_, simulation)
-
-    #         # Calcula score de energia e delay
-    #         energy_score = 1 - (evaluation['total'] / initial_energy)
-    #         delay_score = 1 - (calc_delay(forwarding_) / initial_delay)
-
-    #         # Retorna os scores ponderados com os pesos
-    #         scores.append((energy_score * w_energy) + (delay_score * w_delay))
-    #     return scores
-
-    def fit(population):
-        scores = []
-        for p in population:
-            forwarding_ = framework.forwarding(aig, p)
-            evaluation = evaluate.evaluate(forwarding_, simulation)
-            scores.append(1 - (evaluation['total'] / initial_energy))
-        return scores
-
     # Seleciona os individuos mais adaptados
-    def natural_selection(population, scores):
+    def natural_selection(population):
         half_index = int(len(population) / 2)
-        list_ = sorted(zip(scores, population), key=lambda x: x[0])
-        list_ = list_[half_index:]
-        return [p for s, p in list_], [s for s, p in list_]
+        list_ = sorted(population, key=lambda p: p.score)
+        return list_[half_index:]
 
     # Passo 1 - Definir a população inicial
-    population = initial_population(aig, n_initial_individuals)
+    population = init_population(aig, params.n_initial_individuals)
 
     # Passo 2 - Aplicar funcao fitness na populacao inicial
-    scores = fit(population)
+    fit(population)
 
-    # print('Scores inicais')
-    # print(scores)
+    for i in range(0, params.n_generations):
+        # Encontra melhor e pior
+        best = max(population, key=attrgetter('score'))
+        worst = min(population, key=attrgetter('score'))
+        print("Melhor: " + str(best.score) + " - Pior: " + str(worst.score))
 
-    for i in range(0, n_generations):
         # Reprodução
-        new_generation = reproduce(population, scores, reproduction_rate)
+        new_generation = reproduce(population, params.reproduction_rate, worst.score)
 
         # Mutação
-        new_generation = mutate(new_generation, mutation_rate)
+        new_generation = mutate(new_generation, params.mutation_rate)
 
         # Calcula score dos novos indivíduos
-        scores += fit(new_generation)
+        fit(new_generation)
+        # population += new_generation
+        # Seleciona os mais aptos
+        # population = natural_selection(population)
+
+        # Teste
+        population = natural_selection(population)
+        new_generation = natural_selection(new_generation)
         population += new_generation
 
-        # Seleciona os mais aptos
-        population, scores = natural_selection(population, scores)
 
-        print('Pontuação máxima: ' + str(max(scores)))
+    # Encontra melhor e pior
+    best = max(population, key=attrgetter('score'))
+    worst = min(population, key=attrgetter('score'))
+    print("Melhor: " + str(best.score) + " - Pior: " + str(worst.score))
 
-    max_score = max(scores)
-    max_score_i = scores.index(max_score)
-    return population[max_score_i]
+    return best
